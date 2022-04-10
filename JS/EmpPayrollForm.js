044c9d172680858d0d6644cc6e6da7b224ce8ac4
poonambudde <buddepoonam123@gmail.com>
2022-04-10 23:05:03 +0530

    [Poonam] Added: Set event listner on salary for dynamic input by user

---
+++ JS/EmpPayrollForm.js
@@ -0,0 +1,7 @@
+//Dynamic salary range based on user input
+const salary = document.querySelector('#salary');
+const output = document.querySelector('.salary-output');
+output.textContent = salary.value;
+salary.addEventListener('input', function(){
+    output.textContent = salary.value;
+})
